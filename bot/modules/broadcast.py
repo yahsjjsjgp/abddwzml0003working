1c926eef4f93aa29ba52144bc43062c69a8f5cff
Abdullah Al Noman <101085500+AtrociousMirrorBot@users.noreply.github.com>
2023-04-16 21:54:21 +0600

    Update broadcast.py

--- bot/modules/broadcast.py
+++ bot/modules/broadcast.py
@@ -1,40 +1,81 @@
-from pymongo import MongoClient
-from telegram.ext import CommandHandler, Filters
+import paramiko
+import subprocess
+import telegram
+from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
+from bot import bot, dispatcher
 
-from bot import bot, dispatcher, LOGGER, config_dict
-from bot.helper.telegram_helper.filters import CustomFilters
-from bot.helper.telegram_helper.message_utils import sendMessage
 
+def execute_command(command):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname="example.com", username="username", password="password")
+    stdin, stdout, stderr = client.exec_command(command)
+    output = stdout.read().decode('utf-8')
+    error = stderr.read().decode('utf-8')
+    client.close()
+    return output, error
 
-def broadcast(update, context):
-    reply_to = update.message.reply_to_message
-
-    if not config_dict['DATABASE_URL']:
-        context.bot.send_message(chat_id=update.effective_chat.id, text=f"DATABASE_URL not provided")
-    else:
-        conn = MongoClient(config_dict['DATABASE_URL'])
-        db = conn.mltb
-        users_collection = db.users
-        users_count = db.users.count_documents({})
+def vps(update, context):
+    chat_id = update.effective_chat.id
+    bot.send_message(chat_id=chat_id, text='Please enter your VPS username, IP and password separated by spaces')
     
-        chat_ids = [str(user["_id"]) for user in users_collection.find({}, {"_id": 1})]
+    # Wait for user input
+    def wait_for_input(update, context):
+        user_input = update.message.text.split(' ')
+        if len(user_input) != 3:
+            bot.send_message(chat_id=chat_id, text='Invalid input')
+            return
+        username, ip, password = user_input
 
-        success = 0
+        # Connect to VPS
+        command = f'ssh {username}@{ip}'
+        output, error = execute_command(command)
 
-        for chat_id in chat_ids:
-            try:
-                context.bot.copy_message(chat_id=chat_id, from_chat_id=update.message.chat.id, message_id=reply_to.message_id)
-                success += 1
-            except Exception as err:
-                LOGGER.error(err)
+        # Check for errors
+        if error:
+            bot.send_message(chat_id=chat_id, text=f'Error connecting to VPS: {error}')
+            return
+
+        # Open terminal
+        command = 'gnome-terminal'
+        execute_command(command)
+
+        # Wait for user input
+        bot.send_message(chat_id=chat_id, text='Enter command to run on VPS')
+
+        # Set up handler to wait for user input
+        message_handler = MessageHandler(Filters.text & ~Filters.command, run_command)
+        context.dispatcher.add_handler(message_handler)
+
+        # Save VPS details in context for use in run_command function
+        context.user_data['vps_details'] = {'username': username, 'ip': ip, 'password': password}
+
+    # Set up handler to wait for VPS details input
+    vps_input_handler = MessageHandler(Filters.text & ~Filters.command, wait_for_input)
+    context.dispatcher.add_handler(vps_input_handler)
 
 
-        msg = f"<b>Broadcasting Completed</b>\n"
-        msg += f"<b>Total {users_count} users in Database</b>\n"
-        msg += f"<b>Sucess: </b>{success} users\n"
-        msg += f"<b>Failed: </b>{users_count - success} users"
-        return sendMessage(msg, context.bot, update.message) 
+def run_command(update, context):
+    chat_id = update.effective_chat.id
 
+    # Retrieve VPS details from context
+    vps_details = context.user_data.get('vps_details')
+    if not vps_details:
+        bot.send_message(chat_id=chat_id, text='Error: VPS details not found')
+        return
 
-CommandHandler("broadcast", broadcast, filters=CustomFilters.owner_filter)
-dispatcher.add_handler(CommandHandler('broadcast', broadcast))
+    # Run command on VPS
+    command = f'sshpass -p {vps_details["password"]} ssh {vps_details["username"]}@{vps_details["ip"]} {update.message.text}'
+    output, error = execute_command(command)
+
+    # Send output to Telegram
+    if output:
+        bot.send_message(chat_id=chat_id, text=output)
+    if error:
+        bot.send_message(chat_id=chat_id, text=error)
+
+    # Remove message handler to prevent it from being called again
+    context.dispatcher.remove_handler(context.handler_stack[-1])
+
+vps_handler = CommandHandler('vps', vps)
+dispatcher.add_handler(vps_handler)
