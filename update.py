2bb23fda1f4f4b84b1bcb4700f4a576e9c4b82e3
brooosns <116587987+brooosns@users.noreply.github.com>
2023-06-17 12:36:58 +0530

    Update update.py

--- update.py
+++ update.py
@@ -13,7 +13,7 @@ basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=INFO)
 
 UPSTREAM_REPO = 'https://github.com/SN-Abdullah-Al-Noman/SN_WZML'
-UPSTREAM_BRANCH = 'heroku'
+UPSTREAM_BRANCH = 'master'
 
 if UPSTREAM_REPO is not None:
     if ospath.exists('.git'):
