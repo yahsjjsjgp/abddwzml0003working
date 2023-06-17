002148d300a9ff566fbae85d070390f17d6f8ce4
brooosns <116587987+brooosns@users.noreply.github.com>
2023-06-17 12:28:17 +0530

    Update update.py

--- update.py
+++ update.py
@@ -12,7 +12,7 @@ basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[FileHandler('log.txt'), StreamHandler()],
                     level=INFO)
 
-UPSTREAM_REPO = 'https://github.com/brooosns/abddworking2'
+UPSTREAM_REPO = 'https://github.com/SN-Abdullah-Al-Noman/SN_WZML'
 UPSTREAM_BRANCH = 'heroku'
 
 if UPSTREAM_REPO is not None:
